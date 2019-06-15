da8da7aa91bfbf498912d9dca5838017d139a72a
Aman Bhargava <aman56493@gmail.com>
2019-06-15 22:59:12 +0530

    lint

--- project-euler-17.js
+++ project-euler-17.js
@@ -1,54 +1,52 @@
 const map = {
-    1 : "one",
-    2: "two",
-    3: "three",
-    4: "four",
-    5: "five",
-    6: "six",
-    7: "seven",
-    8: "eight",
-    9: "nine",
-    10: "ten",
-    11: "eleven",
-    12: "twelve",
-    13: "thirteen",
-    14: "fourteen",
-    15: "fifteen",
-    16: "sixteen",
-    17: "seventeen",
-    18: "eighteen",
-    19: "nineteen",
-    20: "twenty",
-    30: "thirty",
-    40: "forty",
-    50: "fifty",
-    60: "sixty",
-    70: "seventy",
-    80: "eighty",
-    90: "ninety",
-    100: "hundred"
+  1: 'one',
+  2: 'two',
+  3: 'three',
+  4: 'four',
+  5: 'five',
+  6: 'six',
+  7: 'seven',
+  8: 'eight',
+  9: 'nine',
+  10: 'ten',
+  11: 'eleven',
+  12: 'twelve',
+  13: 'thirteen',
+  14: 'fourteen',
+  15: 'fifteen',
+  16: 'sixteen',
+  17: 'seventeen',
+  18: 'eighteen',
+  19: 'nineteen',
+  20: 'twenty',
+  30: 'thirty',
+  40: 'forty',
+  50: 'fifty',
+  60: 'sixty',
+  70: 'seventy',
+  80: 'eighty',
+  90: 'ninety',
+  100: 'hundred'
 }
 
 let numbersToStrings = []
 
 const numberToString = (number) => {
-    if (number < 21)
-        return `${map[number]}`
-    if( number < 100)
-        return `${map[number - (number % 10)]} ${map[number % 10] ? map[number % 10] : ' '}`
-    if(number % 100 === 0 && number < 1000) {
-        return `${map[number / 100]} hundred`
-    }
-    if (number <1000) {
-        return `${map[((number % 1000) - (number % 100)) / 100]} hundred and ${numberToString(number % 100)}`
-    }
-    return 'one thousand'
+  if (number < 21) { return `${map[number]}` }
+  if (number < 100) { return `${map[number - (number % 10)]} ${map[number % 10] ? map[number % 10] : ' '}` }
+  if (number % 100 === 0 && number < 1000) {
+    return `${map[number / 100]} hundred`
+  }
+  if (number < 1000) {
+    return `${map[((number % 1000) - (number % 100)) / 100]} hundred and ${numberToString(number % 100)}`
+  }
+  return 'one thousand'
 }
 
-for(let i = 1; i<1001; i++){
-    numbersToStrings[i] = numberToString(i).replace(/\s+/g, '')
+for (let i = 1; i < 1001; i++) {
+  numbersToStrings[i] = numberToString(i).replace(/\s+/g, '')
 }
 
-const reducer = (acc, curr) => acc + curr.length;
+const reducer = (acc, curr) => acc + curr.length
 
 console.log(numbersToStrings.reduce(reducer, 0))
