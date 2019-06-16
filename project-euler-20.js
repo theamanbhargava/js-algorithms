cd204e7a1aa0ac64ce40d9ee540f66e124424cb8
Aman Bhargava <aman56493@gmail.com>
2019-06-17 01:17:56 +0530

    question 2

---
+++ project-euler-20.js
@@ -0,0 +1,50 @@
+const {
+    performance,
+    PerformanceObserver
+} = require('perf_hooks');
+
+const obs = new PerformanceObserver((items) => {
+    items.getEntries().forEach((item) => {
+        console.log('\x1b[32m%s\x1b[0m', item.name, + ' ' + item.duration)
+    })
+})
+obs.observe({entryTypes: ['measure']})
+
+const factors = number => {
+    let factors = []
+    for(let i = 1; i < Math.ceil(number / 2 + 1); i++){
+        if(number % i === 0) {
+            factors.push(i)
+        }
+    }
+    return factors
+}
+
+let factorSum = []
+
+let amicableNumberSum = 0
+
+const sum = number => {
+    let sum =  factors(number).reduce((a, b) => a + b, 0)
+    factorSum[number] = sum
+    let idx = factorSum.indexOf(number);
+    let indices = []
+    while (idx !== -1) {
+        indices.push(idx);
+        idx = factorSum.indexOf(number, idx + 1);
+    }
+
+    if(indices.includes(sum) && factorSum[sum] === number && number !== sum){
+        console.log(`Amicable number detected `, `${number} and ${sum}`)
+        amicableNumberSum += (number + sum)
+    }
+}
+
+performance.mark('Beginning sum')
+for(let i = 1; i < 10000; i++){
+    sum(i)
+}
+console.log("Sum is :", amicableNumberSum)
+performance.mark('Ending sum')
+
+performance.measure('Sum calculation time', 'Beginning sum', 'Ending sum')
