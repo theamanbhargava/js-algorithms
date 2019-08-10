d961344434607610a793c457519346f84a3983e4
Aman Bhargava <aman56493@gmail.com>
2019-08-10 18:13:11 +0530

    code needs optimization

--- project-euler-23.js
+++ project-euler-23.js
@@ -1,12 +1,44 @@
-import { factors } from './project-euler-21'
+const abundantNumbers = []
 
-let abundantNumbers = []
-
-const isAbundant = (number) => factors(number).reduce((a, b) => a + b, 0)
-
-for(let i = 12; i < 28123; i++){
-    if(isAbundant(i)) {
-        abundantNumbers.push(i)
+const factors = number => {
+  const factors = []
+  for (let i = 1; i < Math.ceil(number / 2 + 1); i++) {
+    if (number % i === 0) {
+      factors.push(i)
     }
-    console.log(abundantNumbers)
+  }
+  return factors
 }
+
+const isAbundant = (number) => number < factors(number).reduce((a, b) => a + b, 0)
+
+let sum = 0
+
+const isSumOfAbundantNumbers = (number) => {
+    const index = abundantNumbers.findIndex(n => n >= number / 2)
+    if (index === -1) { return false }
+    if (abundantNumbers[index] === number / 2) {
+        return true
+    }
+    for(let num1 of abundantNumbers) {
+        for(let num2 of abundantNumbers) {
+            if(num1 + num2 === number){
+                return true
+            }
+        }
+    }
+    return false
+}
+
+for (let i = 1; i < 28123; i += 1) {
+  if (isAbundant(i) === true) {
+    abundantNumbers.push(i)
+  }
+    if (!isSumOfAbundantNumbers(i)) {
+        sum += i
+    }
+}
+
+console.log(sum)
+
+console.log(abundantNumbers)
